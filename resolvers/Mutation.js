49b4f26b7dd6ed59b1054d0922bbe445bd8b4db5
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-03-14 02:26:12 +0100

    modularization

---
+++ resolvers/Mutation.js
@@ -0,0 +1,5 @@
+exports.Mutation = {
+  register: (parent, args, context) => {
+      //validate user, encrypt password
+  },
+};
--- schema.js
+++ schema.js
@@ -11,4 +11,20 @@ exports.typeDefs = gql`
     created_at: String!
     username: String!
   }
+  input registerInput {
+    username: String!
+    password: String!
+    email: String!
+  }
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    token: String!
+    created_at: String
+  }
+
+  type Mutation {
+    register(registerInput: registerInput): User!
+  }
 `;
